b8397efbb1a8f29b1173532834f486f1e6620755
Muskan <muskan@email.com>
2026-07-09 20:35:14 +0530

    add countnto1

---
+++ countnto1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class countnto1 {
+    void main(){
+        System.out.println("Reverse Counting from :");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+
+        for(int i =n ; i>=1 ; i--){
+            System.out.println(i);
+
+
+        }
+
+    }
+}

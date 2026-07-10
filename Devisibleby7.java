96d7c9e6dfc7be7de906c555fceaee60480bc0eb
Muskan <muskan@email.com>
2026-07-10 08:01:38 +0530

    Devisibleby7

---
+++ Devisibleby7.java
@@ -0,0 +1,11 @@
+public class Devisibleby7 {
+    void main(){
+
+        for(int i = 50; i<=100; i++){
+            if(i%7 == 0){
+                System.out.println(i);
+            }
+
+        }
+    }
+}

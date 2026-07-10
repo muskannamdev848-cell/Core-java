23c3da7bec98c6fa959a59f17f960b8158c66c8d
Muskan <muskannamdev848@gmail.com>
2026-07-10 08:32:14 +0530

    loopCount1ton program

---
+++ loopCount1ton.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class loopCount1ton {
+    void main(){
+        System.out.println("Counting stopped after getting :");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+
+        for(int i = 1; i <=n ;i++){
+            System.out.println(i);
+        }
+    }
+}

ec9dde7e4ea2bffd0d6a52e92d7af8f334bdf25c
Muskan <muskannamdev848@gmail.com>
2026-07-11 15:10:03 +0530

    PrimeNumber .java

---
+++ PrimeNumber.java
@@ -0,0 +1,15 @@
+public class PrimeNumber {
+    void main(){
+        for(int n = 2; n<=100 ; n++){
+            int count =0;
+            for(int i =1 ; i<=n ; i++){
+                if(n%i == 0){
+                    count++;
+
+                }
+
+            }
+
+        }
+    }
+}

c5384dd8515b5da1416b2fa1497b87f9bc3af8a6
Muskan <muskannamdev848@gmail.com>
2026-07-12 09:04:36 +0530

    pattern1 program

---
+++ pattern1.java
@@ -0,0 +1,13 @@
+public class patter1 {
+    void main(){
+        int n = 3;
+        for(int i= 1; i<=3 ; i++){
+            for(int j = 1;  j<=5; j++){
+
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+    }
+}

e5698a7d3d55ec966c62360c48353e5e042351ba
Muskan <muskannamdev848@gmail.com>
2026-07-12 09:54:30 +0530

    Pattern3 Program

---
+++ Pattern3.java
@@ -0,0 +1,11 @@
+public class Pattern3 {
+    void main(){
+        int n = 5;
+        for(int row = 1;  row<=n; row++){
+            for(int col= 1; col<=(n-row+1); col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

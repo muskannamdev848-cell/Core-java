dc9cf1671b743c8552459fafcfb69a5fec52ac72
Muskan <muskannamdev848@gmail.com>
2026-07-24 19:00:48 +0530

    StringBasics

---
+++ StringBasics.java
@@ -0,0 +1,11 @@
+package String;
+
+public class StringBasics {
+    void main(){
+        String firstName = "Muskan";
+        String lastName = "Namdev";
+        System.out.println(firstName +" " +lastName);
+        System.out.println(firstName.length());
+        System.out.println(firstName.charAt(4));
+    }
+}
